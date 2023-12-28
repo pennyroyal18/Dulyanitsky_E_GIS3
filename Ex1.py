a7817f9f7bbcea2fed217fc8b04980bc28dcf361
FXSO <154833099+pennyroyal18@users.noreply.github.com>
2023-12-28 15:05:27 +0300

    Update Ex1.py
    Shorter

--- Ex1.py
+++ Ex1.py
@@ -1,9 +1,7 @@
 K = int (input('Введите значение K:'))
 N = int (input('Введите значение N:'))
-if K>0:
+if K>0 and N>0:
     for i in range(0,N):
         print(K)
 else:
-    print('K<0')
-if N<0:
-    print('N<0')
+    print('K<0 и N<0')
