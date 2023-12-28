bfdf85cc6fac319d86c7c66c03514d90aae7288e
FXSO <154833099+pennyroyal18@users.noreply.github.com>
2023-12-28 14:59:31 +0300

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,9 @@
+K = int (input('Введите значение K:'))
+N = int (input('Введите значение N:'))
+if K>0:
+    for i in range(0,N):
+        print(K)
+else:
+    print('K<0')
+if N<0:
+    print('N<0')
